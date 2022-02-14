66194e4e690596d925c3614a8d414004320b04ee
Prasad <narkhedkarprasad@gmail.com>
2022-02-14 22:14:30 +0530

    tailwind-css-fix-seven

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  content: ["./pages/**/*.js", "./Components/**/*.js"],
-  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
